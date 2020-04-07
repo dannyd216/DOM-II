666b2b51c0ff0aaa76213301cb8e63e810e07221
dannyd216 <dparrish216@gmail.com>
2020-04-07 13:11:27 -0600

    dparrish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+
+
+
+// * [ ] `mouseover`
+
+
+
+
+// * [ ] `keydown`
+
+
+
+
+// * [ ] `drag / drop`
+
+
+
+
+// * [ ] `load`
+
+
+
+
+// * [ ] `focus`
+
+
+
+
+// * [ ] `resize`
+
+
+
+
+// * [ ] `scroll`
+
+
+
+
+// * [ ] `select`
+
+
+
+
+// * [ ] `dblclick`
+
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+
+
+// * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
