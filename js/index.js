976aa6a4b2dc95271e43238f53720c5e6760bf63
dannyd216 <dparrish216@gmail.com>
2020-04-07 17:23:22 -0600

    dparrish

--- js/index.js
+++ js/index.js
@@ -1,10 +1,58 @@
 // Your code goes here
 
+// const get = (selector) => {
+//     return document.querySelector(selector);
+// }
+
+
+//welcom aler
+
+
+// function welcomeAlert(event) {
+
+// debugger
+
+//     console.log('an even fired at ${event.timestamp}')
+//     return alert('Hi Yall');
+// }
+
+// window.addEventListener('click', welcomeAlert);
+
+// const navChng = document.querySelector('.nav');
+
+// function chngNavColor (event) {
+
+//     navChng.
+// }
+
+
 
 
 // * [ ] `mouseover`
 
 
+// MOUSEOVER FUN BUS IMAGE
+
+const bossImg = document.querySelector('#yellowBus')
+
+function funBusHover(event) {
+
+    console.log(`the mouse has entered`)
+
+
+}
+
+bossImg.addEventListener('mouseover',funBusHover)
+
+
+//mousemove
+
+document.addEventListener('mousemove', event => {
+
+    console.log(`The X: ${event.offsetX} and The Y: ${event.offsetY}`);
+
+
+})
 
 
 // * [ ] `keydown`
